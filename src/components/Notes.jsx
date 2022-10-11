61135ccd3b8c75b952358016ab39c2460d905f07
Namitanv09 <105544442+Namitanv09@users.noreply.github.com>
2022-10-11 23:17:54 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the ReactBootcamp</h1>
+      <p>
+        The Bootcamp is being organised in association with Microsoft Learn
+        Student Ambassadors(MLSA). All the students who complete the Bootcamp
+        with complete attendance and project submission will be rewarded
+        certificates from us.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
